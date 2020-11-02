830e73062a15c5906e96776bfe2ba97c53eafecd
Kevin Lewis <kevin.lewis@vonage.com>
2020-11-02 17:25:37 +0000

    Started to build course page

---
+++ pages/courses/_course/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        {{course.name}}
+    </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    try {
+      const course = await $content(`courses/${params.course}`)
+        .fetch()
+        .catch((err) => {
+          console.log('err', err)
+          error({ statusCode: 404, message: 'Page not found', err })
+        })
+      return { course }
+    } catch (e) {
+      error(e)
+      return false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
