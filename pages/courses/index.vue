a304fbf75bbaf60f375c867cccf62336c7ea015a
Kevin Lewis <kevin.lewis@vonage.com>
2020-11-26 13:19:06 +0000

    /courses now tailwindified

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -1,25 +1,26 @@
 <template>
-  <div>
-    <header class="full-width">
-      <PageHero>Courses from the Vonage Team</PageHero>
-    </header>
-    <div class="Vlt-container">
-      <Breadcrumbs />
-      <nuxt-link
-        v-for="course in courses"
-        :key="course.slug"
-        :to="`/courses/${course.slug}`"
-        class="Vlt-card Vlt-bg-white course"
-      >
-        <img :src="course.thumbnail" :alt="course.title" />
-        <div class="info">
-          <h1 property="headline">
-            {{ course.title }}
-          </h1>
-          <h2 class="summary">{{ course.summary }}</h2>
-        </div>
-      </nuxt-link>
-    </div>
+  <div class="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
+    <Breadcrumbs />
+    <section class="courses">
+      <div class="grid grid-cols-1 gap-6 md:grid-cols-2">
+        <nuxt-link
+          v-for="(course, i) in courses"
+          :key="i"
+          :to="course.path"
+          class="flex flex-col overflow-hidden rounded-lg shadow-lg"
+        >
+          <img
+            :src="course.thumbnail"
+            :alt="course.title"
+            class="rounded-b-none"
+          />
+          <div class="p-6 bg-white">
+            <h2 class="text-lg mb-2 block">{{ course.title }}</h2>
+            <p>{{ course.summary }}</p>
+          </div>
+        </nuxt-link>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -36,27 +37,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-.header {
-  text-align: center;
-  padding: 2em 0;
-}
-.full-width {
-  width: 100vw;
-}
-.Vlt-container {
-  padding: 0 1em;
-}
-img {
-  border-radius: 6px;
-  margin-bottom: 1em;
-}
-h1 {
-  margin-bottom: 1rem;
-}
-h2 {
-  margin-top: 0;
-  margin-bottom: 0;
-}
-</style>
