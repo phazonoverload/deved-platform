fe38427d79b7efd23c84921764676d9b6b843036
Kevin Lewis <kevin.lewis@vonage.com>
2020-11-26 12:08:40 +0000

    Fixed immediate issues with volta icons

--- components/Courses/Module.vue
+++ components/Courses/Module.vue
@@ -13,9 +13,18 @@
           class="Vlt-list-item"
         >
           <span class="Vlt-list-item__icon">
-            <svg class="Vlt-icon Vlt-icon--smaller Vlt-blue-light">
-              <use
-                xlink:href="../../node_modules/@vonagevolta/volta2/dist/symbol/volta-icons.svg#Vlt-icon-check-circle-negative"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              class="stroke-current stroke-1 icon-size"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
           </span>
--- components/Courses/PrevNext.vue
+++ components/Courses/PrevNext.vue
@@ -1,6 +1,53 @@
 <template>
   <div v-if="prev || next" class="links">
     <div class="prev">
+      <nuxt-link
+        v-if="prev"
+        :to="prev.slug"
+        class="button button--pill button--primary"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          class="stroke-current stroke-2 icon-size mr-2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
+          />
+        </svg>
+        Previous</nuxt-link
+      >
+      <span v-else>{{ start }}</span>
+    </div>
+
+    <div class="next">
+      <nuxt-link
+        v-if="next"
+        :to="next.slug"
+        class="button button--pill button--primary"
+      >
+        Next
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          class="stroke-current stroke-2 icon-size ml-2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          /></svg
+      ></nuxt-link>
+      <span v-else>{{ end }}</span>
+    </div>
+    <!-- <div class="prev">
       <NuxtLink
         v-if="prev"
         :to="prev.slug"
@@ -13,8 +60,8 @@
         >{{ prev.title }}
       </NuxtLink>
       <span v-else>{{ start }}</span>
-    </div>
-    <div class="next">
+    </div> -->
+    <!-- <div class="next">
       <NuxtLink
         v-if="next"
         :to="next.slug"
@@ -28,7 +75,7 @@
         </svg>
       </NuxtLink>
       <span v-else>{{ end }}</span>
-    </div>
+    </div> -->
   </div>
 </template>
 
